6b7fb0e4f7996dcfc5cb8896ed767139215ca36a
chiarapedrico <chiaraapedrico@icloud.com>
2024-04-26 14:56:05 -0300

    Payment, one-to-one association

--- src/main/java/com/example/course/config/TestConfig.java
+++ src/main/java/com/example/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.example.course.entities.Category;
 import com.example.course.entities.Order;
 import com.example.course.entities.OrderItem;
+import com.example.course.entities.Payment;
 import com.example.course.entities.Product;
 import com.example.course.entities.User;
 import com.example.course.entities.enums.OrderStatus;
@@ -78,6 +79,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-07-22T21:21:22Z"), o3);
+		o3.setPayment(pay1);
+		
+		orderRepository.save(o3);
 		
 		
 	}
